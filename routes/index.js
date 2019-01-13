81bc663d7d14d5ef643493e67b93d4de55e89722
dh1027.shin <bn2091n@naver.com>
2019-01-13 18:33:32 +0900

    member, mercenary

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,99 @@
-var express = require('express');
-var path = require('path');
-var router = express.Router();
-var fs = require('fs');
-/* GET home page. */
-router.get('/user', function(req, res){
-    //let path = require('path');
-  let user_list = fs.readFileSync(__dirname + '/../files/users.txt', 'utf8').split(';');
-  user_list.sort();
+let express = require('express');
+let path = require('path');
+let router = express.Router();
+let fs = require('fs');
+const file = __dirname + '/../files/member.txt';
 
-  var cur = path.resolve('.');
-  res.render('index', {
-    cur: cur,
-    user_list:  user_list
+router.get('/', function(req, res){
+   res.render('index');
+});
+
+router.get('/index', function(req, res){
+    res.render('index');
+});
+/* GET home page. */
+router.get('/member', function(req, res){
+  let member = fs.readFileSync(file, 'utf8').split(';');
+
+  res.render('member', {
+    member: member
   });
 });
 
-function create_team(num, user_list) {
-    let team_arr = new Array(num);
-    let check = new Array(user_list.length);
+router.post('/member', function(req, res){
+  const {new_member} = req.body;
 
-    for(var i = 0; i < num; i++){
+  let member = fs.readFileSync(file, 'utf8').split(';');
+  member.push(new_member);
+  member.sort();
+
+  let data = "";
+  for(let i = 0; i < member.length; i++){
+      if(member[i]){
+          data += member[i];
+          if(i != member.length - 1){
+              data += ';';
+          }
+      }
+  }
+
+  fs.writeFile(file, data, 'utf8', function(error){
+  });
+  res.end();
+});
+
+router.delete('/member/:delete_name', function(req, res){
+    let delete_name = req.params.delete_name;
+    let member = fs.readFileSync(file, 'utf8').split(';');
+    let data = "";
+    for(let i = 0; i < member.length; i++){
+        if(!member[i]) continue;
+        else if(member[i] != delete_name){
+            data += member[i];
+            data += ';';
+        }
+    }
+
+    if(data[data.length - 1] == ';'){
+        data = data.substr(0, data.length - 1);
+    }
+
+    fs.writeFile(file, data, 'utf8', function(error){
+    });
+    res.end();
+});
+
+
+function create_team(num, mercenary, member) {
+    let team_arr = new Array(num);
+
+    for(let i = 0; i < mercenary; i++){
+        member.push("용병" + (i + 1));
+    }
+
+    let check = new Array(member.length);
+
+    for(let i = 0; i < num; i++){
         team_arr[i] = new Array();
     }
 
-    for(var i = 0; i < user_list.length; i++){
+    for(let i = 0; i < member.length; i++){
         check[i] = false;
     }
 
     let count = 0;
 
-    while(count < user_list.length) {
+    while(count < member.length) {
         let flag = true;
         let team_number = count % num;
 
         while (flag) {
-            var index = Math.floor(Math.random() * user_list.length);
+            var index = Math.floor(Math.random() * member.length);
             if (!check[index]) {
                 check[index] = true;
                 count++;
                 flag = false;
-                team_arr[team_number].push(user_list[index]);
+                team_arr[team_number].push(member[index]);
             }
         }
     }
@@ -47,17 +101,22 @@ function create_team(num, user_list) {
     return team_arr;
 }
 
-router.post('/team', function(req, res) {
-    const {num, user_list} = req.body;
-    console.log(user_list);
+router.get('/create_team', function(req, res){
+    let member = fs.readFileSync(file, 'utf8').split(';');
 
-    //let today = new Date().toISOString().substr(0, 10).replace('T', ' ');
-    //let fs_today = fs.readFileSync(__dirname + '/../files/today.txt', 'utf8').split(';');
+    res.render('create_team', {
+        member: member
+    });
+});
 
-    let team_arr = create_team(num, user_list);
-    console.log(team_arr);
+router.post('/create_team', function(req, res) {
+    let {team_num, mercenary_num, member} = req.body;
+    if(!mercenary_num)
+        mercenary_num = 0;
 
-    res.render('team', {
+    let team_arr = create_team(team_num, mercenary_num, member);
+
+    res.render('result_team', {
         team_arr: team_arr
     });
 });
